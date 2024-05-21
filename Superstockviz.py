e78d173efb5d7d5ac1ed0b6b871774fd795d0a35
Braden Griebel <bgriebel@uw.edu>
2024-05-21 14:19:42 -0700

    Added annotated news charts to the streamlit app

--- Superstockviz.py
+++ Superstockviz.py
@@ -2,16 +2,20 @@
 # Standard Library Imports
 from __future__ import annotations
 import datetime
+import os
 
 # External Imports
 import numpy as np
 import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Local Imports
 import sviz
 
 # Setup/Data Reading
+# Streamlit setup
+st.set_page_config(layout="wide")
 
 # Read in stock data
 stock_df = pd.read_csv("./data/stockprice.csv", header=[0,1], index_col=0)
@@ -26,10 +30,16 @@ for attr in np.unique(stock_df.columns.get_level_values(0)):
 stock_data = pd.concat(stock_df_list, axis=1).reset_index().merge(sp500info_df, how="inner", left_on="ticker", right_on="Symbol")
 stock_data["Date"] = pd.to_datetime(stock_data["Date"], format="%Y-%m-%d").dt.date
 
-
+# Determine which annotations are available
+annotated_tickers = [x.split(".")[0] for x in os.listdir("./data/news_annotated")]
 
 # Start of Page
 st.header("Welcome to SuperStockViz!")
+
+
+st.header("Adaptive Stock Viewer")
+
+
 # Input list of tickers, can be seperated in any way
 ticker_str = st.text_input("Enter tickers")
 # Parse tickers
@@ -67,9 +77,6 @@ if len(date_tuple)!=2:
 
 start, end = date_tuple
 
-type(start)
-
-
 
 
 
@@ -77,15 +84,26 @@ type(start)
 st.altair_chart(sviz.stock_chart(stock_data=stock_data[
     (stock_data["Date"]>=start)&(stock_data["Date"]<=end)
     ][stock_data["GICS Sector"].isin(industry_filter)], 
-                                 tickers=ticker_list, 
-                                 width=650, 
-                                 upper_height=300, 
-                                 lower_height=100, 
-                                 date_col="Date",
-                                 ticker_col="ticker", 
-                                 price_col="Close", 
-                                 open_col="Open", 
-                                 close_col="Close", 
-                                 high_col="High", 
-                                 low_col="Low", 
-                                 aggregate_func="mean"))
+                                tickers=ticker_list, 
+                                width=650, 
+                                upper_height=300, 
+                                lower_height=100, 
+                                date_col="Date",
+                                ticker_col="ticker", 
+                                price_col="Close", 
+                                open_col="Open", 
+                                close_col="Close", 
+                                high_col="High", 
+                                low_col="Low", 
+                                aggregate_func="mean"),
+                                use_container_width=True)
+    
+
+st.header("Annotated Stock Price Data")
+
+annotated_ticker = st.selectbox("Select a Ticker of Interest",[x.split(".")[0] for x in annotated_tickers])
+with open(f"./data/news_annotated/{annotated_ticker}.html", 'r') as f:
+    annotated_news_chart = f.read()
+components.html(annotated_news_chart, height=800, width=1000)
+    
+
